0c944ad6f49f503901a9a7076a20d820a29e84c9
G-Corporationcare <gcorporationcare@gmail.com>
2023-02-19 12:43:57 +0100

    fix: Update common library

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 const isGithubActions = process.env.GITHUB_ACTIONS || false;
 let assetPrefix = '';
-let basePath = '/';
+let basePath = '';
 if (isGithubActions) {
   // trim off `<owner>/`
   const repo = (process.env.GITHUB_REPOSITORY || '').replace(/.*?\//, '');
