181cb553decdc1954824405f92941cce07c1918a
G-Corporationcare <gcorporationcare@gmail.com>
2023-02-19 11:43:23 +0100

    feat: Add github build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+let assetPrefix = '';
+let basePath = '/';
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = (process.env.GITHUB_REPOSITORY || '').replace(/.*?\//, '');
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
